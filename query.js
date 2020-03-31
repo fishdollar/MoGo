8386662f2b6c0c0ab5efc6bbabf0dd3cfe18bf83
fishdollar <53010294+fishdollar@users.noreply.github.com>
2020-04-01 01:39:55 +0300

    minor edits

--- query.js
+++ query.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$('div.rollout').click(function(){
+	$("div.rollout").click(function(){
 		if ($(this).next().css('display') == 'block') {
 			$(this).next().slideUp(300);
 			$('div.rollout').children('img:last-of-type').css({transform: 'scale(1, 1)'});
@@ -10,5 +10,8 @@ $(document).ready(function(){
 			$(this).children('img:last-of-type').css({transform: 'scale(1, -1)'});
 			$('div.rollout').not(this).children('img:last-of-type').css({transform: 'scale(1, 1)'});
 		}
+	});
+	$('#piccontainer').children("img").mouseover(function(){
+
 	});
 });
