d6e1616c0276ff7a5742cd31345f8c7050e263b8
fishdollar <53010294+fishdollar@users.noreply.github.com>
2020-04-03 05:27:21 +0300

    edits

--- query.js
+++ query.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
 	$("div.rollout").click(function(){
 		if ($(this).next().css('display') == 'block') {
 			$(this).next().slideUp(300);
-			$('div.rollout').children('img:last-of-type').css({transform: 'scale(1, 1)'});
+			$('div.rollout').children('img:last-of-type').css({"transform": 'scale(1, 1)'});
 		}
 		else {
 			$('p.rollout').slideUp(300);
 			$(this).next().slideDown(300);
-			$(this).children('img:last-of-type').css({transform: 'scale(1, -1)'});
-			$('div.rollout').not(this).children('img:last-of-type').css({transform: 'scale(1, 1)'});
+			$(this).children('img:last-of-type').css({"transform": 'scale(1, -1)'});
+			$('div.rollout').not(this).children('img:last-of-type').css({"transform": 'scale(1, 1)'});
 		}
 	});
 	$('#piccontainer').children("img").mouseover(function(){
